b25abcb90ae9007eb6467cdefffa473b8091f9fa
Tkag0001 <trinhhuynhthinhkhang@gmail.com>
2023-08-02 15:39:55 +0700

    Change to test

--- readJsonFile.js
+++ readJsonFile.js
@@ -4,11 +4,12 @@ const display = document.querySelector("#display_data");
 const getData = async () => {
     const res = await fetch(apiEndpoint)
     const data = await res.json();
-    return data
+    return data;
 }
-
+let dataJson;
 const displayUsers = async () => {
     const payload = await getData();
+    dataJson = payload;
     console.log(payload)
 }
 
